825a36e18f84b59607e3bfd0676c81e3816adcbf
Osouzaa <gabrielsouzaalves750@gmail.com>
2024-11-05 19:04:40 -0300

    docs: created documentation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,24 @@
-import { NestFactory } from '@nestjs/core'
-import { AppModule } from './app.module'
-import { env } from './env'
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { env } from './env';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-  await app.listen(env.PORT)
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('CloudForge API')
+    .setDescription('Documentação da API do Kanban da CloudForge')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  app.useGlobalPipes(new ValidationPipe())
+
+  await app.listen(env.PORT);
 }
-bootstrap()
+bootstrap();
